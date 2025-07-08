7893423a985e133d7980042ec8fe9251d6b3edaa
Joe Polny <joepolny@gmail.com>
2025-07-08 17:34:24 -0400

    chore(dev-deps): fix ruff and mypy errors

--- src/algokit_subscriber/utils.py
+++ src/algokit_subscriber/utils.py
@@ -1,6 +1,5 @@
 import logging
 from collections.abc import Callable
-from typing import TypeVar
 
 from algosdk.encoding import encode_address as sdk_encode_address
 
@@ -12,14 +11,11 @@ def range_inclusive(start: int, end: int) -> list[int]:
     return list(range(start, end + 1))
 
 
-T = TypeVar("T")
-
-
-def chunk_array(arr: list[T], size: int) -> list[list[T]]:
+def chunk_array[T](arr: list[T], size: int) -> list[list[T]]:
     return [arr[i : i + size] for i in range(0, len(arr), size)]
 
 
-def reduce(array: list[T], callback: Callable[[T, T], T]) -> T:
+def reduce[T](array: list[T], callback: Callable[[T, T], T]) -> T:
     iterator = iter(array)
     accumulator: T = next(iterator)
 
